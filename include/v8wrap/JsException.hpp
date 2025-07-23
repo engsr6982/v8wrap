bbbb27f72d0bd1408b53059261e954e075a40399
engsr6982 <pwxg286704@163.com>
2025-07-23 19:51:15 +0800

    feat(JsException): Enhance exception handling and stacktrace

--- include/v8wrap/JsException.hpp
+++ include/v8wrap/JsException.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "Global.hpp"
-#include <stdexcept>
+#include <exception>
+#include <string>
 #include <v8-exception.h>
 #include <v8-persistent-handle.h>
 
@@ -8,7 +9,7 @@
 namespace v8wrap {
 
 
-class JsException : public std::runtime_error {
+class JsException final : public std::exception {
 public:
     enum class Type {
         Unknown = -1, // JavaScript 侧抛出的异常为 Unknown
@@ -22,24 +23,34 @@ public:
     V8WRAP_DISALLOW_COPY_AND_MOVE(JsException)
 
     explicit JsException(v8::TryCatch const& tryCatch);
-    explicit JsException(Type type, std::string const& message = {});
-    explicit JsException(std::string message, Type = Type::Error);
-
-    [[nodiscard]] char const* what() const noexcept override;
+    explicit JsException(std::string message, Type type = Type::Error);
 
     [[nodiscard]] Type type() const noexcept;
 
+    [[nodiscard]] char const* what() const noexcept override;
+
     [[nodiscard]] std::string message() const noexcept;
 
-    [[nodiscard]] std::string stackTrace() const noexcept;
+    [[nodiscard]] std::string stacktrace() const noexcept;
+
+    /**
+     * @brief 将异常重新抛出到 JavaScript 侧
+     */
+    void rethrowToRuntime() const;
 
 public:
+    /**
+     * @brief 将 v8::TryCatch 中的异常重新抛出为 JsException
+     */
     static void rethrow(v8::TryCatch const& tryCatch);
 
 private:
-    Type                      mType;
-    std::string               mMessage;
-    v8::Global<v8::Exception> mException;
+    void extractMessage() const noexcept;
+    void makeException() const;
+
+    Type                          mType{Type::Unknown};
+    mutable std::string           mMessage{};
+    mutable v8::Global<v8::Value> mException{};
 };
 
 
--- src/JsException.cc
+++ src/JsException.cc
@@ -1,26 +1,103 @@
 #include "v8wrap/JsException.hpp"
+#include "v8wrap/JsReference.hpp"
+#include "v8wrap/JsRuntimeScope.hpp"
+#include "v8wrap/JsValue.hpp"
+#include <exception>
+#include <v8-exception.h>
+#include <v8-local-handle.h>
+#include <v8-persistent-handle.h>
+#include <v8-primitive.h>
+#include <v8-value.h>
+#include <v8.h>
+
 
 namespace v8wrap {
 
 
-JsException::JsException(v8::TryCatch const& tryCatch) 
-: mType(Type::Unknown),
-mMessage(tryCatch.Message()),
-mStackTrace(tryCatch.StackTrace())
-{}
+JsException::JsException(v8::TryCatch const& tryCatch) : std::exception() {
+    auto isolate = JsRuntimeScope::currentRuntimeIsolateChecked();
+    mException   = v8::Global<v8::Value>(isolate, tryCatch.Exception());
+}
 
-JsException::JsException(Type type, std::string const& message) {}
+JsException::JsException(std::string message, Type type) : std::exception(), mType(type), mMessage(std::move(message)) {
+    makeException(); // null exception, make it
+}
 
-JsException::JsException(std::string message, Type) {}
+JsException::Type JsException::type() const noexcept { return mType; }
 
-char const* JsException::what() const noexcept {}
+char const* JsException::what() const noexcept {
+    extractMessage();
+    return mMessage.c_str();
+}
 
-JsException::Type JsException::type() const noexcept {}
+std::string JsException::message() const noexcept {
+    extractMessage();
+    return mMessage;
+}
 
-std::string JsException::message() const noexcept {}
+std::string JsException::stacktrace() const noexcept {
+    auto&& [isolate, ctx] = JsRuntimeScope::currentIsolateAndContextChecked();
 
-std::string JsException::stackTrace() const noexcept {}
+    auto vtry = v8::TryCatch{isolate}; // noexcept
 
+    auto stack = v8::TryCatch::StackTrace(ctx, mException.Get(isolate));
+    if (!stack.IsEmpty()) {
+        v8::String::Utf8Value ut8{isolate, stack.ToLocalChecked()};
+        if (auto str = *ut8) {
+            return str;
+        }
+    }
+    return "[ERROR: Could not get stacktrace]";
+}
+
+void JsException::rethrowToRuntime() const {
+    auto isolate = JsRuntimeScope::currentRuntimeIsolateChecked();
+    isolate->ThrowException(mException.Get(isolate));
+}
+
+void JsException::extractMessage() const noexcept {
+    if (!mMessage.empty()) {
+        return;
+    }
+    auto isolate = JsRuntimeScope::currentRuntimeIsolateChecked();
+    auto vtry    = v8::TryCatch{isolate};
+
+    auto msg = v8::Exception::CreateMessage(isolate, mException.Get(isolate));
+    if (!msg.IsEmpty()) {
+        Local<JsString> jsStr{msg->Get()};
+        mMessage = jsStr.toString().getValue();
+        return;
+    }
+    mMessage = "[ERROR: Could not get exception message]";
+}
+
+void JsException::makeException() const {
+    auto isolate = JsRuntimeScope::currentRuntimeIsolateChecked();
+
+    v8::Local<v8::Value> exception;
+    {
+        switch (mType) {
+        case Type::Unknown:
+        case Type::Error:
+            exception = v8::Exception::Error(v8::String::NewFromUtf8(isolate, mMessage.c_str()).ToLocalChecked());
+            break;
+        case Type::RangeError:
+            exception = v8::Exception::RangeError(v8::String::NewFromUtf8(isolate, mMessage.c_str()).ToLocalChecked());
+            break;
+        case Type::ReferenceError:
+            exception =
+                v8::Exception::ReferenceError(v8::String::NewFromUtf8(isolate, mMessage.c_str()).ToLocalChecked());
+            break;
+        case Type::SyntaxError:
+            exception = v8::Exception::SyntaxError(v8::String::NewFromUtf8(isolate, mMessage.c_str()).ToLocalChecked());
+            break;
+        case Type::TypeError:
+            exception = v8::Exception::TypeError(v8::String::NewFromUtf8(isolate, mMessage.c_str()).ToLocalChecked());
+            break;
+        }
+    }
+    mException = v8::Global<v8::Value>(isolate, exception);
+}
 
 void rethrow(v8::TryCatch const& tryCatch) {
     if (tryCatch.HasCaught()) {
