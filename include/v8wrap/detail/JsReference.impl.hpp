b6977643c36d87043a8736e36d947b616fc9b1e8
engsr6982 <pwxg286704@163.com>
2025-07-23 17:46:10 +0800

    feat(v8wrap): Implement Global and Weak template classes

---
+++ include/v8wrap/detail/JsReference.impl.hpp
@@ -0,0 +1,112 @@
+#pragma once
+#include "v8wrap/JsReference.hpp"
+#include "v8wrap/JsRuntimeScope.hpp"
+
+namespace v8wrap {
+
+
+// Global<T>
+template <typename T>
+Global<T>::Global() noexcept : impl{} {}
+
+template <typename T>
+Global<T>::Global(Local<T> const& val) : impl(JsRuntimeScope::currentRuntime(), val) {
+    auto* runtime = JsRuntimeScope::currentRuntime();
+    assert(runtime && "Global<T> must be created inside a JsRuntimeScope");
+}
+
+template <typename T>
+Global<T>::Global(Weak<T> const& val) : impl(JsRuntimeScope::currentRuntime(), val) {}
+
+template <typename T>
+Global<T>::Global(Global<T>&& other) noexcept : impl(std::move(other.impl)) {}
+
+template <typename T>
+Global<T>& Global<T>::operator=(Global<T>&& other) noexcept {
+    if (&other != this) {
+        impl = std::move(other.impl);
+    }
+    return *this;
+}
+
+template <typename T>
+Global<T>::~Global() = default;
+
+template <typename T>
+Local<T> Global<T>::get() const {
+    return impl.template get<T>();
+}
+
+template <typename T>
+Local<JsValue> Global<T>::getValue() const {
+    return impl.getValue();
+}
+
+template <typename T>
+bool Global<T>::isEmpty() const {
+    return impl.isEmpty();
+}
+
+template <typename T>
+void Global<T>::reset() {
+    impl.reset();
+}
+
+
+// Weak<T>
+template <typename T>
+Weak<T>::Weak() noexcept : impl{} {
+    impl.makeWeak();
+}
+
+template <typename T>
+Weak<T>::Weak(Local<T> const& val) : impl(JsRuntimeScope::currentRuntime(), val) {
+    impl.makeWeak();
+    auto* runtime = JsRuntimeScope::currentRuntime();
+    assert(runtime && "Weak<T> must be created inside a JsRuntimeScope");
+}
+
+template <typename T>
+Weak<T>::Weak(Global<T> const& val) : impl(JsRuntimeScope::currentRuntime(), val) {
+    impl.makeWeak();
+}
+
+template <typename T>
+Weak<T>::Weak(Weak<T>&& other) noexcept : impl(std::move(other.impl)) {
+    impl.makeWeak();
+}
+
+template <typename T>
+Weak<T>& Weak<T>::operator=(Weak<T>&& other) noexcept {
+    if (&other != this) {
+        impl = std::move(other.impl);
+        impl.makeWeak();
+    }
+    return *this;
+}
+
+template <typename T>
+Weak<T>::~Weak() = default;
+
+template <typename T>
+Local<T> Weak<T>::get() const {
+    return impl.template get<T>();
+}
+
+template <typename T>
+Local<JsValue> Weak<T>::getValue() const {
+    return impl.getValue();
+}
+
+template <typename T>
+bool Weak<T>::isEmpty() const {
+    return impl.isEmpty();
+}
+
+template <typename T>
+void Weak<T>::reset() {
+    impl.reset();
+}
+
+
+} // namespace v8wrap
--- include/v8wrap/internal/V8GlobalRef.hpp
+++ include/v8wrap/internal/V8GlobalRef.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "V8TypeAlias.hpp"
 #include "v8wrap/Global.hpp"
+#include "v8wrap/JsReference.hpp"
 #include "v8wrap/JsRuntime.hpp"
 #include "v8wrap/Types.hpp"
 
@@ -19,14 +20,15 @@ public:
     V8WRAP_DISALLOW_COPY(V8GlobalRef);
 
     V8GlobalRef() = default;
-    explicit V8GlobalRef(JsRuntime* rt, Local<T> local) : mRuntime(rt), mHandle(rt->mIsolate, rt->unwrap(local)) {}
+    explicit V8GlobalRef(JsRuntime* rt, Local<T> local)
+    : mRuntime(rt),
+      mHandle(rt->mIsolate, v8wrap::JsRuntime::unwrap(local)) {}
 
     V8GlobalRef(V8GlobalRef&& other) noexcept {
         mRuntime       = other.mRuntime;
         mHandle        = std::move(other.mHandle);
         other.mRuntime = nullptr;
     }
-
     V8GlobalRef& operator=(V8GlobalRef&& other) noexcept {
         if (this != &other) {
             mRuntime       = other.mRuntime;
@@ -35,13 +37,23 @@ public:
         }
         return *this;
     }
-
-    ~V8GlobalRef() = default;
+    ~V8GlobalRef() { reset(); }
 
     void makeWeak() {
         if (!mHandle.IsEmpty()) {
             mHandle.SetWeak();
         }
+    }
+
+    Local<T> get() const { return Local<T>{mHandle.Get(mRuntime->mIsolate)}; }
+
+    Local<JsValue> getValue() const { return Local<JsValue>{mHandle.Get(mRuntime->mIsolate).template As<v8::Value>()}; }
+
+    bool isEmpty() const { return mHandle.IsEmpty(); }
+
+    void reset() {
+        mHandle.Reset();
+        mRuntime = nullptr;
     }
 };
 
