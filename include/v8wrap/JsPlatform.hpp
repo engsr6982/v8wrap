e7d96023171da7e2366f2ff9483fe989af47a712
engsr6982 <pwxg286704@163.com>
2025-07-24 16:52:15 +0800

    feat(JsPlatform): Add platform initialization and runtime management

--- include/v8wrap/JsPlatform.hpp
+++ include/v8wrap/JsPlatform.hpp
@@ -1,13 +1,30 @@
 #pragma once
 #include "v8wrap/Global.hpp"
-
+#include "v8wrap/Types.hpp"
+#include <memory>
+#include <mutex>
+#include <v8-platform.h>
 
 namespace v8wrap {
 
 
 class JsPlatform {
+    std::unique_ptr<v8::Platform> mPlatform;
+    std::vector<JsRuntime*>       mRuntimes;
+    mutable std::mutex            mMutex;
+
+    void initPlatform();
+
 public:
     V8WRAP_DISALLOW_COPY(JsPlatform);
+
+    JsPlatform();                                                // default, create default platform
+    explicit JsPlatform(std::unique_ptr<v8::Platform> platform); // use existing platform
+    virtual ~JsPlatform();
+
+    [[nodiscard]] JsRuntime* newRuntime();
+
+    void removeRuntime(JsRuntime* runtime, bool destroyRuntime = true);
 };
 
 
--- src/JsPlatform.cc
+++ src/JsPlatform.cc
@@ -1,3 +1,57 @@
 #include "v8wrap/JsPlatform.hpp"
+#include "v8-isolate.h"
+#include "v8wrap/JsRuntime.hpp"
+#include <libplatform/libplatform-export.h>
+#include <libplatform/libplatform.h>
+#include <libplatform/v8-tracing.h>
+#include <memory>
+#include <mutex>
+#include <v8-initialization.h>
+#include <v8-platform.h>
+#include <vector>
 
-namespace v8wrap {}
+
+namespace v8wrap {
+
+
+JsPlatform::JsPlatform() : mPlatform(v8::platform::NewDefaultPlatform()) { initPlatform(); }
+JsPlatform::JsPlatform(std::unique_ptr<v8::Platform> platform) : mPlatform(std::move(platform)) { initPlatform(); }
+
+void JsPlatform ::initPlatform() {
+    v8::V8::InitializePlatform(mPlatform.get());
+    v8::V8::Initialize();
+}
+
+JsPlatform::~JsPlatform() {
+    std::lock_guard<std::mutex> lock(mMutex);
+    v8::V8::Dispose();
+    v8::V8::DisposePlatform();
+}
+
+JsRuntime* JsPlatform::newRuntime() {
+    std::lock_guard<std::mutex> lock(mMutex);
+    v8::Isolate::CreateParams   params;
+    params.array_buffer_allocator = v8::ArrayBuffer::Allocator::NewDefaultAllocator();
+
+    v8::Isolate* isolate = v8::Isolate::New(params);
+
+    JsRuntime* runtime;
+    {
+        v8::Isolate::Scope isolate_scope(isolate);
+        v8::HandleScope    handle_scope(isolate);
+        runtime = new JsRuntime(isolate);
+    }
+    mRuntimes.emplace_back(runtime);
+    return runtime;
+}
+
+void JsPlatform::removeRuntime(JsRuntime* runtime, bool destroyRuntime) {
+    std::lock_guard<std::mutex> lock(mMutex);
+    std::erase_if(mRuntimes, [runtime](JsRuntime* r) { return r == runtime; });
+
+    if (destroyRuntime) {
+        runtime->destroy();
+    }
+}
+
+} // namespace v8wrap
