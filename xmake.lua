0335fc0f723807c6ca022f5630397673fbfab9b7
engsr6982 <pwxg286704@163.com>
2025-07-24 15:52:53 +0800

    fix(构建): 修复Linux平台下测试目标的链接问题

--- xmake.lua
+++ xmake.lua
@@ -70,23 +70,32 @@ target("v8wrap_test")
     set_languages("cxx20")
     add_includedirs("include") -- add v8wrap include dir
     add_files("test/**.cc")
-    add_packages("catch2")
+
+    -- 确保Catch2库正确链接
+    if is_plat("linux") then
+        -- 在Linux上，需要明确指定链接顺序
+        add_packages("catch2", {links = {"Catch2", "Catch2Main"}})
+    else
+        add_packages("catch2")
+    end
 
     if is_plat("windows") then 
         add_cxflags("/utf-8", "/W4", "/sdl")
         add_syslinks("winmm", "advapi32") -- add required system libraries
     elseif is_plat("linux") then
         add_cxflags("-fPIC", "-stdlib=libc++", {force = true})
-        add_syslinks("dl", "pthread")
+        add_ldflags("-stdlib=libc++", {force = true})
+        add_syslinks("dl", "pthread", "c++", "m")
     end
 
     -- v8 headers and libs
     add_includedirs(get_config("v8_include_dir"))
-    
+
     -- 直接添加静态库文件而不是使用add_links
     if is_plat("linux") then
-        add_linkgroups(get_config("v8_static_lib"))
-        add_syslinks("dl", "pthread")
+        -- 在Linux上，链接顺序很重要，先链接应用程序，再链接库
+        add_ldflags("-Wl,--whole-archive", get_config("v8_static_lib"), "-Wl,--no-whole-archive", {force = true})
+        add_syslinks("dl", "pthread", "c++", "m")
     else
         add_links(get_config("v8_static_lib"))
     end
